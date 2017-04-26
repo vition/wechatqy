c2bdd41928d50919adfbd7a67580209e879c3936
vition <369709991@qq.com>
2017-04-26 10:51:49 +0800

    编写wechatqy文件

--- wechatqy.py
+++ wechatqy.py
@@ -2,4 +2,22 @@
 
 class wehatqy(object):
 	"微信企业号的类"
-	def __init__(self)	
+	def __init__(self):
+		pass
+	#config
+	def conf(self,corpid,corpsecret):
+		self.corpid=corpsecret
+		self.corpsecret=corpsecret
+	#access_token
+	def access_token(self,token=""):
+		"token为空，取token，否则设置token"
+		if hasattr(self,token):
+			if token!="":
+				self.token=token
+			else:
+				return self.token
+		else:
+			print "请先配置corpid和corpsecret"
+
+print __name__
+
